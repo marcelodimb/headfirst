f72939f60d856ae49c0258bb8728cf9ec09d891f
Marcelo Bueno <marcelorezendebueno@gmail.com>
2015-05-01 15:57:13 -0300

    Foi inserido uma descrição em cada função do index.

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 def hello(name):
+    """ Imprime uma saudação junto com o nome informado. """
     print "Hello " + name + "!"
 
 def square_number(num):
+    """ Retorna o quadrado do numero informado. """
     return num ** 2
     
 hello("Marcelo")
