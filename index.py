82c5d2dfb240ef227ed4a9b46d44cfb6818505d1
Marcelo Bueno <marcelorezendebueno@gmail.com>
2015-05-01 17:53:34 -0300

    Limpou os caracteres de acentuação e removeu as chamadas pelas funções.

--- index.py
+++ index.py
@@ -1,12 +1,9 @@
 import math
 
 def hello(name):
-    """ Imprime uma saudação junto com o nome informado. """
+    """ Imprime uma saudacao junto com o nome informado. """
     print "Hello " + name + "!"
 
 def cubic_number(num):
     """ Retorna o cubo do numero informado. """
     return math.pow(num, 3)
-
-hello("Marcelo")
-
