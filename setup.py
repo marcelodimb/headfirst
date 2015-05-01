9dc0a67369495a328e1b98ba48958bbb71f279e3
Marcelo Bueno <marcelorezendebueno@gmail.com>
2015-05-01 18:18:13 -0300

    Criação da primeira distribuição.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name 			= 'cubic',
+    version 		= '1.0.0',
+    py_modules 		= ['index'],
+    author 			= 'marcelodimb',
+    author_email	= 'marcelodimb@gmail.com',
+    url				= 'http://igeo-server.igeo.ufrj.br/~bueno/',
+    description		= 'A simple cubic number printer.'
+)
