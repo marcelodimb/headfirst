c0f5e4bde4536d32beb2d49c1eaf37b92fd882da
Marcelo Bueno <marcelorezendebueno@gmail.com>
2015-05-01 18:32:01 -0300

    Correção da distribuição para incluir o arquivo cubic.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name 			= 'cubic',
     version 		= '1.0.0',
-    py_modules 		= ['index'],
+    py_modules 		= ['cubic'],
     author 			= 'marcelodimb',
     author_email	= 'marcelodimb@gmail.com',
     url				= 'http://igeo-server.igeo.ufrj.br/~bueno/',
